1b94383c73f8fd9047fc881fef973e03620732e3
elliesch <ellianna.schwab@macaulay.cuny.edu>
2017-05-24 16:57:32 -0400

    updated tabs

--- Tests/test_typing_kit.py
+++ Tests/test_typing_kit.py
@@ -3,20 +3,20 @@ from TypeFinder import *
 
 class TestData(unittest.TestCase):
 
-    def setUp(self):
-    	'''
-    	This defines data to test
-    	'''
+    def setUp(self):
+        '''
+        This defines data to test
+        '''
         self.data = Data('test_L3.fits')
 
     def test_init(self):
-    	'''
-    	This tests that the J-H-K arrays have been sorted properly by checking their boundary conds.
-    	'''
+        '''
+        This tests that the J-H-K arrays have been sorted properly by checking their boundary conds.
+        '''
 
-        self.assertTrue([wlength >= 0.87 and wlength <= 1.39 for wlength in self.data.wavelength_J])
-        self.assertTrue([wlength >= 1.41 and wlength <= 1.89 for wlength in self.data.wavelength_H])
-        self.assertTrue([wlength >= 1.91 and wlength <= 2.39 for wlength in self.data.wavelength_K])
+        self.assertTrue([0.87 <= wlength <= 1.39 for wlength in self.data.wavelength_J])
+        self.assertTrue([1.41 <= wlength <= 1.89 for wlength in self.data.wavelength_H])
+        self.assertTrue([1.91 <= wlength <= 2.39 for wlength in self.data.wavelength_K])
 
 
 ### FUTURE TESTS ###

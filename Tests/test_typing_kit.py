2b28f2e3583707473ece18cd0b178520102438c5
elliesch <ellianna.schwab@macaulay.cuny.edu>
2017-05-24 17:09:46 -0400

    for now placeholder tests

--- Tests/test_typing_kit.py
+++ Tests/test_typing_kit.py
@@ -1,22 +1,22 @@
 import unittest
 from TypeFinder import *
 
-class TestData(unittest.TestCase):
-
-    def setUp(self):
-        '''
-        This defines data to test
-        '''
-        self.data = Data('test_L3.fits')
-
-    def test_init(self):
-        '''
-        This tests that the J-H-K arrays have been sorted properly by checking their boundary conds.
-        '''
-
-        self.assertTrue([0.87 <= wlength <= 1.39 for wlength in self.data.wavelength_J])
-        self.assertTrue([1.41 <= wlength <= 1.89 for wlength in self.data.wavelength_H])
-        self.assertTrue([1.91 <= wlength <= 2.39 for wlength in self.data.wavelength_K])
+# class TestData(unittest.TestCase):
+#
+#     def setUp(self):
+#         '''
+#         This defines data to test
+#         '''
+#         self.data = Data('test_L3.fits')
+#
+#     def test_init(self):
+#         '''
+#         This tests that the J-H-K arrays have been sorted properly by checking their boundary conds.
+#         '''
+#
+#         self.assertTrue([0.87 <= wlength <= 1.39 for wlength in self.data.wavelength_J])
+#         self.assertTrue([1.41 <= wlength <= 1.89 for wlength in self.data.wavelength_H])
+#         self.assertTrue([1.91 <= wlength <= 2.39 for wlength in self.data.wavelength_K])
 
 
 ### FUTURE TESTS ###
